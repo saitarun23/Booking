d6bac9ed2fa2da3c599de249332a093162d1deed
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2025-11-30 14:49:58 +0530

    done

---
+++ Admin/backend/user-micro-service-app/src/main/java/com/user/user_micro_service_app/UserMicroServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.user.user_micro_service_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroServiceAppApplication.class, args);
+	}
+
+}
